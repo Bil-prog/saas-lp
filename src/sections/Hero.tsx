8f64e212b188b7c1fb54c85b66b4ebbdbf7aec36
Bilha <bilhamwengi34@gmail.com>
2025-01-13 20:34:23 +0300

    product showcase section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,7 @@
 import { ArrowRight } from "lucide-react";
 import cog from "@/assets/cog.png";
 import cylinder from "@/assets/cylinder.png";
-import noodle from "@/assets/noodle.png"
+import noodle from "@/assets/noodle.png";
 import Image from "next/image";
 
 export const Hero = () => {
@@ -10,14 +10,12 @@ export const Hero = () => {
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
-            <div className="inline-flex text-sm border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-              Version 2.0 is here
-            </div>
+            <div className="tag">Version 2.0 is here</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
               Pathway to productivity
             </h1>
             <p className="text-xl text-[#010D3E] tracking-tight mt-6">
-              Celebtate the joy of accomplishment with an app designed to track
+              Celebrate the joy of accomplishment with an app designed to track
               your progress, motivate your efforts and celebrate yur success.
             </p>
             <div className="flex gap-1 items-center mt-[30px]">
@@ -29,9 +27,24 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image src={cog} alt="cogImage" className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"/>
-            <Image src={cylinder} alt="cylinder" width={220} height={220} className="hidden md:block -top-8 -left-32 md:absolute"/>
-            <Image src={noodle} alt="noodle" width={220} className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"/>
+            <Image
+              src={cog}
+              alt="cogImage"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src={cylinder}
+              alt="cylinder"
+              width={220}
+              height={220}
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
+            <Image
+              src={noodle}
+              alt="noodle"
+              width={220}
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
           </div>
         </div>
       </div>
