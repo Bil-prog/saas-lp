875439eee77824dcb153b76d215a4b37cbf4611c
Bilha <bilhamwengi34@gmail.com>
2025-01-14 14:52:44 +0300

    logo ticker animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import acme from "@/assets/logo-acme.png";
 import quantum from "@/assets/logo-quantum.png";
 import echo from "@/assets/logo-echo.png";
@@ -5,20 +7,31 @@ import celestial from "@/assets/logo-celestial.png";
 import pulse from "@/assets/logo-pulse.png";
 import apex from "@/assets/logo-apex.png";
 import Image from "next/image";
+import {motion} from 'motion/react'
 
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
     <div className="container">
       <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-        <div className="flex gap-14 flex-none">
+        <motion.div className="flex gap-14 flex-none pr-14"
+        animate={{translateX: "-50%"}}
+        transition={{duration: 20, repeat: Infinity, ease:"linear", repeatType:"loop"}}
+        >
           <Image src={acme} alt="acme" className="logo-ticker-img" />
           <Image src={quantum} alt="acme" className="logo-ticker-img" />
           <Image src={echo} alt="acme" className="logo-ticker-img" />
           <Image src={celestial} alt="acme" className="logo-ticker-img" />
           <Image src={pulse} alt="acme" className="logo-ticker-img" />
           <Image src={apex} alt="acme" className="logo-ticker-img" />
-        </div>
+          {/* second set */}
+          <Image src={acme} alt="acme" className="logo-ticker-img" />
+          <Image src={quantum} alt="acme" className="logo-ticker-img" />
+          <Image src={echo} alt="acme" className="logo-ticker-img" />
+          <Image src={celestial} alt="acme" className="logo-ticker-img" />
+          <Image src={pulse} alt="acme" className="logo-ticker-img" />
+          <Image src={apex} alt="acme" className="logo-ticker-img" />
+        </motion.div>
       </div>
     </div>
     </div>
